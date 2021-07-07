53cdb26c3d8774105638734af9ed6d4b2a684961
prithviraj-jadeja <prithvirajjadeja.319@gmail.com>
2021-07-07 15:50:24 +0530

    First commit

---
+++ Finalcall.java
@@ -0,0 +1,14 @@
+package repopract;
+
+public class Finalcall {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("prithprint");
+		System.out.println("prhpr");
+		System.out.println("prith");
+		System.out.println("print");
+
+	}
+
+}
